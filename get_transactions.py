9c72be67e869b38836cb37e53ea8215a6c0114a0
raylu <lurayl@gmail.com>
2020-05-29 23:02:56 -0700

    insert transactions into db

--- get_transactions.py
+++ get_transactions.py
@@ -7,8 +7,18 @@ def main():
 	with db:
 		cur = db.execute('SELECT access_token FROM plaid_item LIMIT 1')
 		[[access_token]] = cur.fetchall()
-	for t in plaid.iter_transactions(access_token):
-		print(t['transaction_id'], t['date'])
+
+		transactions = []
+		for t in plaid.iter_transactions(access_token):
+			amount = int(t['amount'] * 100)
+			category = ', '.join(t['category'])
+			transactions.append((t['transaction_id'], t['date'], t['name'], amount, category))
+			print('%s %-50s %6d %s' % (t['date'], t['name'], amount, category))
+		db.executemany('''
+			INSERT INTO plaid_transaction (transaction_id, date, name, amount, category) VALUES (?, ?, ?, ?, ?)
+			ON CONFLICT(transaction_id) DO UPDATE SET
+			date = excluded.date, name = excluded.name, amount = excluded.amount, category = excluded.category
+		''', transactions)
 
 if __name__ == '__main__':
 	main()
