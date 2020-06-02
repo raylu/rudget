425b48fa46a751eb8ad47caa57c182cca05842ae
raylu <lurayl@gmail.com>
2020-06-01 22:35:51 -0700

    dedupe account IDs

--- get_transactions.py
+++ get_transactions.py
@@ -4,21 +4,35 @@ from db import db
 import plaid
 
 def main():
+	seen = set()
 	with db:
 		cur = db.execute('SELECT access_token FROM plaid_item')
 		while True:
 			row = cur.fetchone()
 			if row is None:
 				break
-			process_item(row['access_token'])
+			process_item(row['access_token'], seen)
 
-def process_item(access_token):
+def process_item(access_token, seen):
+	auth = plaid.auth(access_token)
+	account_ids = []
+	for account in auth['numbers']:
+		account_number = account['account']
+		if account_number in seen:
+			print('skipping', account['account_id'])
+		else:
+			seen.add(account_number)
+			account_ids.append(account['account_id'])
+	if len(account_ids) == 0:
+		return
+
+	print('getting transactions for', account_ids)
 	transactions = []
-	for t in plaid.iter_transactions(access_token):
+	for t in plaid.iter_transactions(access_token, account_ids):
 		amount = int(t['amount'] * 100)
 		category = ', '.join(t['category'])
 		transactions.append((t['transaction_id'], t['date'], t['name'], amount, category))
-		print('%s %-50s %9d %s' % (t['date'], t['name'], amount, category))
+		#print('%s %-50s %9d %s' % (t['date'], t['name'], amount, category))
 	db.executemany('''
 		INSERT INTO plaid_transaction (transaction_id, date, name, amount, category) VALUES (?, ?, ?, ?, ?)
 		ON CONFLICT(transaction_id) DO UPDATE SET
--- plaid.py
+++ plaid.py
@@ -8,22 +8,22 @@ rs = requests.Session()
 
 def exchange_token(public_token):
 	data = _post('/item/public_token/exchange', json={
-		'client_id': config.plaid.client_id,
-		'secret': config.plaid.development,
 		'public_token': public_token,
 	})
 	return data['item_id'], data['access_token']
 
-def iter_transactions(access_token):
+def auth(access_token):
+	return _post('/auth/get', json={'access_token': access_token})
+
+def iter_transactions(access_token, account_ids):
 	offset = 0
 	while True:
 		data = _post('/transactions/get', json={
-			'client_id': config.plaid.client_id,
-			'secret': config.plaid.development,
 			'access_token': access_token,
 			'start_date': '2000-01-01',
 			'end_date': str(datetime.date.today()),
 			'options': {
+				'account_ids': account_ids,
 				'offset': offset,
 				'count': 500,
 			},
@@ -36,6 +36,10 @@ def iter_transactions(access_token):
 			break
 
 def _post(endpoint, json):
+	json.update({
+		'client_id': config.plaid.client_id,
+		'secret': config.plaid.development,
+	})
 	response = rs.post('https://%s.plaid.com%s' % (config.plaid.environment, endpoint), json=json)
 	response.raise_for_status()
 	return response.json()
