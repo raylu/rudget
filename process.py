db141ae7be19716e98a0cf3519abccb76abdd316
raylu <lurayl@gmail.com>
2020-06-01 20:15:39 -0700

    periodic classifier

--- process.py
+++ process.py
@@ -2,6 +2,7 @@
 
 import collections
 from dataclasses import dataclass
+import datetime
 
 from db import db
 import plaid
@@ -15,7 +16,35 @@ def main():
 
 	payee_tuples = sorted(payees.items(), key=lambda item: len(item[1]), reverse=True)
 	for name, transactions in payee_tuples:
-		print(name, len(transactions))
+		if len(transactions) < 3:
+			continue
+		first = datetime.datetime.strptime(transactions[0]['date'], '%Y-%m-%d')
+
+		last_date = datetime.datetime.strptime(transactions[1]['date'], '%Y-%m-%d')
+		last_interval = last_date - first
+		last_amount = transactions[1]['amount']
+		periodic = non_periodic = 0
+		for t in transactions[2:]:
+			date = datetime.datetime.strptime(t['date'], '%Y-%m-%d')
+			interval = date - last_date
+			if is_periodic(interval, last_interval, t['amount'], last_amount):
+				periodic += 1
+			else:
+				non_periodic += 1
+
+			last_interval = interval
+			last_date = date
+			last_amount = t['amount']
+		print(name, periodic, non_periodic)
+
+def is_periodic(interval, last_interval, amount, last_amount):
+	if interval.days < 20:
+		return False
+	if abs(interval - last_interval).days > 7:
+		return False
+	if abs(amount - last_amount) / last_amount > 0.4:
+		return False
+	return True
 
 def iter_transactions():
 	with db:
