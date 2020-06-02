cfbb263f086578e407309ab1e4521da0fd8164b3
raylu <lurayl@gmail.com>
2020-06-01 22:01:24 -0700

    use all plaid_items

--- get_transactions.py
+++ get_transactions.py
@@ -5,20 +5,25 @@ import plaid
 
 def main():
 	with db:
-		cur = db.execute('SELECT access_token FROM plaid_item LIMIT 1')
-		[[access_token]] = cur.fetchall()
+		cur = db.execute('SELECT access_token FROM plaid_item')
+		while True:
+			row = cur.fetchone()
+			if row is None:
+				break
+			process_item(row['access_token'])
 
-		transactions = []
-		for t in plaid.iter_transactions(access_token):
-			amount = int(t['amount'] * 100)
-			category = ', '.join(t['category'])
-			transactions.append((t['transaction_id'], t['date'], t['name'], amount, category))
-			print('%s %-50s %9d %s' % (t['date'], t['name'], amount, category))
-		db.executemany('''
-			INSERT INTO plaid_transaction (transaction_id, date, name, amount, category) VALUES (?, ?, ?, ?, ?)
-			ON CONFLICT(transaction_id) DO UPDATE SET
-			date = excluded.date, name = excluded.name, amount = excluded.amount, category = excluded.category
-		''', transactions)
+def process_item(access_token):
+	transactions = []
+	for t in plaid.iter_transactions(access_token):
+		amount = int(t['amount'] * 100)
+		category = ', '.join(t['category'])
+		transactions.append((t['transaction_id'], t['date'], t['name'], amount, category))
+		print('%s %-50s %9d %s' % (t['date'], t['name'], amount, category))
+	db.executemany('''
+		INSERT INTO plaid_transaction (transaction_id, date, name, amount, category) VALUES (?, ?, ?, ?, ?)
+		ON CONFLICT(transaction_id) DO UPDATE SET
+		date = excluded.date, name = excluded.name, amount = excluded.amount, category = excluded.category
+	''', transactions)
 
 if __name__ == '__main__':
 	main()
