69f858109d08ce7e55dbfef4082791a1b6effe9d
raylu <90059+raylu@users.noreply.github.com>
2020-07-22 22:49:44 -0700

    eslint

--- static/plaid.js
+++ static/plaid.js
@@ -1,14 +1,14 @@
 'use strict';
+/* global Plaid */
 (() => {
 	async function plaidSuccess(public_token, metadata) {
-		console.log('metadata', metadata);
 		await fetch('/plaid_access_token', {
 			method: 'POST',
 			headers: {'Content-Type': 'application/json'},
 			redirect: 'error',
 			body: JSON.stringify({'public_token': public_token}),
 		});
-	};
+	}
 
 	const plaid = Plaid.create({
 		clientName: 'rudget',
